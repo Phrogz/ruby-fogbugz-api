0ebff68a344dae7d86f18917d4393ed21d92164e
George Murphy <george@jupiter.(none)>
2008-11-26 15:30:37 -0600

    more helpers...still getting used to git

--- fogbugz_api.rb
+++ fogbugz_api.rb
@@ -1,1 +1,10 @@
-require "lib/fogbugz_api"
+require 'rubygems' rescue nil
+require 'nokogiri'
+require 'net/http'
+require 'lib/fogbugz_api'
+require 'lib/fogbugz/query_builder'
+require 'lib/fogbugz/xml_response'
+require 'lib/fogbugz/http_transport'
+require 'lib/fogbugz/api'
+require 'lib/fogbugz/api_helpers'
+require 'lib/fogbugz/fogbugz_http_api'
--- lib/fogbugz/api_helpers.rb
+++ lib/fogbugz/api_helpers.rb
@@ -13,9 +13,30 @@ module FogBugz
       end
       
       def filters
-       []
+        execute_cmd('listFilters')['filters']['filter'].inject(Hash.new({})) do |resp, f|
+          name = f['$']
+          resp[name] = f.keys.inject({}) {|a, v| a[v.gsub('@','')] = f[v] if v =~ /^@.+/; a}
+          resp[name].merge!({'name'=> name})
+          resp
+        end
       end
-    
+      
+      def projects(fWrite=false, ixProject=nil)
+        bld = builder do |b|
+          b.fWrite(1.to_s) if fWrite
+          b.ixProject(ixProject) if ixProject
+        end
+        execute_cmd('listProjects', bld)
+      end
+      
+      private
+      # TODO finish this function for compatibility with old version of api wrapper
+      def list_process(items, idx)
+        items.inject(Hash.new({})) do |resp, item|
+          name = item[idx]['$']
+          resp[name] = item.each {|k, v| }
+        end
+      end
     end
   end
 end
